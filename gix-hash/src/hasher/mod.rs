f141c132b9bb721fdd96ecb595f96fe874fda998
Emily <hello@emily.moe>
2025-03-29 19:12:28 +0000

    wip: use `std::hint::unreachable_unchecked`

--- gix-hash/src/hasher/mod.rs
+++ gix-hash/src/hasher/mod.rs
@@ -39,7 +39,17 @@ impl Hasher {
         match self.0.try_finalize() {
             CollisionResult::Ok(digest) => Ok(crate::ObjectId::Sha1(digest.into())),
             CollisionResult::Mitigated(_) => {
-                unreachable!("`CollisionResult::Mitigated` is only returned when `safe_hash()` is on");
+                // SAFETY: `CollisionResult::Mitigated` is only
+                // returned when `safe_hash()` is on. `Hasher`’s field
+                // is private, and we only construct it in the
+                // `Default` instance, which turns `safe_hash()` off.
+                //
+                // As of Rust 1.84.1, the compiler can’t figure out
+                // this function cannot panic without this.
+                #[allow(unsafe_code)]
+                unsafe {
+                    std::hint::unreachable_unchecked()
+                }
             }
             CollisionResult::Collision(digest) => Err(Error::CollisionAttack {
                 digest: crate::ObjectId::Sha1(digest.into()),
